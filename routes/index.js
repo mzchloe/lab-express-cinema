cafe5e88ae412586bfb7dfd976fbc350a2a0c75f
Jeanet Lin <linett@gmail.com>
2022-01-28 15:39:33 +0100

    iteration 4

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const Movie = require('../models/movie.model.js') //.. go back to parent folder, and the next folder
+//get our schema model 
+const Movie = require('../models/movie.model.js') 
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
-//route to movies.hbs after clicking 'Check the movies!'
-router.get('/movies', async (req, res) => { // --> /movies is the URL 
-    const movies = await Movie.find() //model we define in line 3, which takes the model from the file
-    console.log(movies)
-    res.render('movies', {movies}) //'movies' = hbs file -- {movies}argument with the array with the information
+//route to movies.hbs when clicking 'Check the movies!'
+router.get('/movies', async (req, res) => { // --> '/movies' is the URL part
+     
+    //model we define in line 3, which takes the model from the file
+    const movies = await Movie.find()
+    //console.log(movies)
+    res.render('movies', {movies}) 
+    //'movies' = hbs file
+    //{movies} second argument, the array with the information
 })
 
+//route to details of the selected movie from /movies page
+    router.get('/movie/:id', async (req, res) => {
+
+        //we get the details from a specific ID from our model
+        const details = await Movie.findById(req.params.id) 
+
+        //render to our details.hbs, {details} showing the info of the selected movie id  
+        res.render('details', {details})
+    })
 module.exports = router;
